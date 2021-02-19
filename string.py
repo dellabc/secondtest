95042833d5a92b5aa322b8a6e6a090bcd54c1628
kevin <trungkien.dtvt@gmail.com>
2021-02-19 10:02:36 +0700

    Add 4 files

---
+++ string.py
@@ -0,0 +1,77 @@
+# Chuỗi trần	
+print(r'OK, neu mot ngay nao do')
+
+# Nối chuỗi
+strA = "Nguyen"
+strB = "Trung Kien"
+strC = strA + " " + strB
+print(strC)
+strD = strC*5
+print(strD)
+
+# In
+
+strK = "Kien"
+strChild1 = "ie"
+strChild2 = "k"
+strL = strChild1 in strK
+strM = strChild2 in strK
+print(strL)
+print(strM)
+ 
+# Indexing
+strF = strK[2]
+strG = strK[-1]
+strH = strK[len(strK)-3]
+print(strF)
+print(strG)
+print(strH)
+
+# Cut string
+strCut1 = strK[1:3]
+strCut2 = strK[1:len(strK)]
+strCut3 = strK[1:None]
+strCut4 = strK[None:None]
+strCut5 = strK[None:2]
+strCut6 = strK[None:]
+strCut7 = strK[:]
+
+strCut8 = strA[1:4:2]
+
+print(strCut1)
+print(strCut2)
+print(strCut3)
+print(strCut4)
+print(strCut5)
+print(strCut6)
+print(strCut7)
+print(strCut8)
+
+
+# Ép kiểu
+a = "7.1"
+b = "8"
+c = "-10"
+d = 7.1
+
+var_a = float(a)
+var_b = int(b)
+var_c = int(c)
+var_d = int(d)
+
+
+print(var_a)
+print(var_b)
+print(var_c)
+print(var_d)
+print(type(var_c))
+print(type(d))
+print(type(var_d))
+
+# Change string
+
+str1 = "Kiên"
+str2 = str1[0:0:-1]
+str3 = "Đ" + str1[1:]
+print(str2)
+print(str3)
---
+++ string_2.py
@@ -0,0 +1,65 @@
+stra = 'My name is %s'%('Kien')
+strb = 'My friends: %s, %s, %s.' %('Jim','Tom','Harry')
+numa = '%d' %(7)
+numb = '%d - %d = %d' %(10,3,7)
+numc = '%.3f is a float' %(3.14141414)
+print(stra)
+print(strb)
+print(numa)
+print(numb)
+print(numc)
+
+# Class Something
+class Something:
+	def __repr__(self):
+		return 'This is __repr__'
+	def __str__(self):
+		return 'This is __str__'
+
+sth = Something()
+print(type(sth))
+print(sth)
+sth
+
+
+
+# f string
+
+str1 = f'Tet 2021'
+print(str1)
+
+# format
+k = 'Toi la {}? Day la {}?'.format('ai','dau')
+l = 'Toi la {1}? Day la {0}?'.format('ai','dau')
+m = 'Toi la {{0}}? Day la {1} {1}?'.format('ai','dau')
+m = 'Toi la {who}? Day la {where}?'.format(who = 'ai',where ='dau')
+print(k)
+print(l)
+print(m)
+
+x = '{:^20}'.format('Hello!')
+x1 = '{:*^30}'.format('Hello!')
+y = '{:@<20}'.format('Hello!')
+z = '{:->20}'.format('Hello!')
+xyz = 'This is{:-^40} test'.format('Hello!')
+print(x)
+print(x1)
+print(y)
+print(z)
+print(xyz)
+
+# Table
+
+# ---- phần định dạng 
+row_1 = '+ {:-<6} + {:-^15} + {:->10} +'.format('', '', '') 
+row_2 = '| {:<6} | {:^15} | {:>10} |'.format('ID', 'Ho va ten',  'Noi sinh') 
+row_3 = '| {:<6} | {:^15} | {:>10} |'.format('123', 'Yui Hatano',  'Japanese') 
+row_4 = '| {:<6} | {:^15} | {:>10} |'.format('6969',  'Sunny Leone', 'Canada') 
+row_5 = '+ {:-<6} + {:-^15} + {:->10} +'.format('', '', '') 
+ 
+# ---- phần xuất kết quả 
+print(row_1) 
+print(row_2) 
+print(row_3) 
+print(row_4) 
+print(row_5) 
